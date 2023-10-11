0438e7fba090637f38c18182cbfbe8a3e7aa9913
gonzalolencina <gonzalo.lencina@roamler.com>
2023-10-11 14:05:23 +0200

    changes building and pushing the docker image

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,41 @@
 # Use a base image with Python
-FROM gcr.io/datamechanics/spark:platform-3.1-dm14
+FROM apache/spark-py
 
 # Install PySpark
-RUN pip3 install pyspark
-RUN pip3 install findspark
+# RUN pip3 install pyspark
+ENV PYTHONUSERBASE /tmp/.local
+RUN pip3 install --user pyspark
+RUN pip3 install --user findspark
+RUN pip3 install --user numpy
 
-COPY main.py .
-ENV AWS_ACCESS_KEY_ID=AKIAVP5IO2RVCXZKULAT
-ENV AWS_SECRET_ACCESS_KEY=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
-CMD [ "python", "main.py" ]
+# Install Hadoop AWS dependencies
+USER root
+RUN wget -O /opt/spark/jars/hadoop-aws-3.3.4.jar https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.4/hadoop-aws-3.3.4.jar
+RUN wget -O /opt/spark/jars/aws-java-sdk-1.12.558.jar https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk/1.12.558/aws-java-sdk-1.12.558.jar
+RUN wget -O /opt/spark/jars/aws-java-sdk-core-1.12.558.jar https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-core/1.12.558/aws-java-sdk-core-1.12.558.jar
+RUN wget -O /opt/spark/jars/aws-java-sdk-bundle-1.12.558.jar https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.558/aws-java-sdk-bundle-1.12.558.jar
+RUN ls /opt/spark/jars/
+
+# Set Hadoop configuration for S3 access
+ENV HADOOP_VERSION 3.3.4
+ENV AWS_SDK_VERSION 1.12.558
+
+# This env variables should be in the task definition
+ENV AWS_ACCESS_KEY_ID=AKIAVP5IO2RVI3BXZKQK
+ENV AWS_SECRET_ACCESS_KEY=4hixBNYb5ARYmf1sNNbexxOeIFMqJs7V2q1Gc74K
+ENV ENV=dev
+ENV USER_TOPIC_1=like
+ENV USER_TOPIC_2=love
+# Possible values = sample.json, twitter-sample.json
+ENV DATASET=twitter-sample.json
+
+# Specify S3A as the file system
+ENV SPARK_CONF_DIR /conf
+RUN mkdir -p $SPARK_CONF_DIR
+RUN echo "spark.hadoop.fs.s3a.access.key $AWS_ACCESS_KEY_ID" > $SPARK_CONF_DIR/spark-defaults.conf
+RUN echo "spark.hadoop.fs.s3a.secret.key $AWS_SECRET_ACCESS_KEY" >> $SPARK_CONF_DIR/spark-defaults.conf
+
+
+COPY trend-analysis/main.py .
+COPY trend-analysis/main.py /opt/spark/work-dir/
+CMD [ "python", "trend-analysis/main.py" ]
--- trend-analysis/main.py
+++ trend-analysis/main.py
@@ -11,6 +11,7 @@ from pyspark.sql.functions import col, split, explode, window, lower, regexp_rep
     count, lit, first, sum
 from datetime import datetime, timedelta
 
+
 def get_spark_session():
     """
     This method create a spark session base on execution type.
@@ -25,10 +26,12 @@ def get_spark_session():
             .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
             .config("spark.driver.maxResultSize", "5g")
             .config("spark.sql.autoBroadcastJoinThreshold", 10485760)
-            .config("spark.hadoop.fs.s3a.access.key", os.getenv('AWS_ACCESS_KEY_ID'))
-            .config("spark.hadoop.fs.s3a.secret.key", os.getenv('AWS_SECRET_ACCESS_KEY'))
+            .config("spark.hadoop.fs.s3a.access.key", os.environ.get('AWS_ACCESS_KEY_ID'))
+            .config("spark.hadoop.fs.s3a.secret.key", os.environ.get('AWS_SECRET_ACCESS_KEY'))
             .config("spark.hadoop.fs.s3a.aws.credentials.provider",
                     "com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
+            .config("spark.hadoop.mapreduce.outputcommitter.factory.scheme.s3a",
+                    "org.apache.hadoop.fs.s3a.commit.S3ACommitterFactory")
             .config("spark.sql.legacy.timeParserPolicy", "LEGACY")
             .config("spark.version", "3.1.2")
             .getOrCreate()
@@ -40,6 +43,8 @@ def get_spark_session():
             # .config("hive.metastore.connect.retries", 3)
             .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
             .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+            .config("spark.hadoop.mapreduce.outputcommitter.factory.scheme.s3a",
+                    "org.apache.hadoop.fs.s3a.commit.S3ACommitterFactory")
             # .config("spark.driver.maxResultSize", "5g")
             .config("spark.sql.legacy.timeParserPolicy", "LEGACY")
             # .config("spark.sql.autoBroadcastJoinThreshold", 10485760)
@@ -49,79 +54,60 @@ def get_spark_session():
     return spark
 
 
-def preprocess_json_data(data):
-    # Define a function to convert date/time strings to datetime objects
-    def parse_datetime(dt_str):
-        try:
-            return datetime.strptime(dt_str, "%a %b %d %H:%M:%S %z %Y")
-        except ValueError:
-            return None
-
-    # Your preprocessing logic goes here
-    if "created_at" in data["user"]:
-        data["user"]["created_at"] = parse_datetime(data["user"]["created_at"])
-
-    # Handle nullable values
-    nullable_fields = ["user.following", "user.follow_request_sent", "in_reply_to_status_id", "in_reply_to_user_id",
-                       "contributors", "user.utc_offset", "url", "place", "coordinates"]
-
-    # Nullable fields are handled by setting their values to None if they are missing in the input data.
-    for field_path in nullable_fields:
-        fields = field_path.split(".")
-        current_data = data
-        for field in fields[:-1]:
-            current_data = current_data.get(field, {})
-        current_data[fields[-1]] = current_data.get(fields[-1], None)
-
-    return data
-
-
-def clean_data(_sc, _bucket_name, _folder_path):
+def clean_data(_sc, _bucket_name, _folder_path, _dataset):
     # Read the JSON data from S3
-    date_format = "EEE MMM dd HH:mm:ss Z yyyy"
-    # Clean text data (e.g., remove special characters and extra whitespace)
-    # Convert 'created_at' column to datetime
-    initialDf = _sc.read.json(f"s3a://{_bucket_name}/{_folder_path}") \
-        .select("created_at", "text") \
-        .dropna(subset='text') \
-        .withColumn("created_at", to_timestamp(col("created_at"), date_format)) \
-        .withColumn("text", lower(regexp_replace(col("text"), r'[^a-zA-Z0-9\s]', ''))) \
-        .withColumnRenamed("text", "tweet_text") \
-        .withColumnRenamed("created_at", "timestamp") \
-        .orderBy(col("timestamp").desc())
+    initialDf = _sc.read.json(f"s3a://{_bucket_name}/{_folder_path}")
+    if _dataset == "sample.json":
+        current_format = "EEE MMM dd HH:mm:ss Z yyyy"
+        initialDf = initialDf.select("created_at", "text") \
+            .withColumn("created_at", to_timestamp(col("created_at"), current_format)) \
+            .dropna(subset='text').dropna(subset='created_at') \
+            .withColumn("text", lower(regexp_replace(col("text"), r'[^a-zA-Z0-9\s]', ''))) \
+            .withColumnRenamed("text", "tweet_text") \
+            .withColumnRenamed("created_at", "timestamp") \
+            .withColumn("timestamp", date_format(col("timestamp"), "yyyy-MM-dd")) \
+            .orderBy(col("timestamp").desc())
 
-    return initialDf
+        initialDf.show()
+        return initialDf, datetime(2011, 2, 27)
+    else:
+        current_format = "EEE, dd MMM yyyy HH:mm:ss Z"
+        initialDf = initialDf.select(col("twitter.created_at").alias("created_at"), col("twitter.text").alias("text")) \
+            .withColumn("created_at", to_timestamp(col("created_at"), current_format)) \
+            .dropna(subset='text').dropna(subset='created_at') \
+            .withColumn("text", lower(regexp_replace(col("text"), r'[^a-zA-Z0-9\s]', ''))) \
+            .withColumnRenamed("text", "tweet_text") \
+            .withColumnRenamed("created_at", "timestamp") \
+            .withColumn("timestamp", date_format(col("timestamp"), "yyyy-MM-dd")) \
+            .orderBy(col("timestamp").desc())
+
+        initialDf.select("timestamp").distinct().show()
+        return initialDf, datetime(2012, 2, 5)
 
 
 if __name__ == '__main__':
     sc = get_spark_session()
-    parser = argparse.ArgumentParser(description='Trend analysis module')
-    parser.add_argument('--env', required=False, default='dev')
+    # parser = argparse.ArgumentParser(description='Trend analysis module')
+    # parser.add_argument('--env', required=False, default='dev')
     # parser.add_argument('--user_topic_1', required=True, default=None)
     # parser.add_argument('--user_topic_2', required=True, default=None)
-    args, unknown = parser.parse_known_args()
+    # args, unknown = parser.parse_known_args()
+    print(os.environ.get('ENV'))
+    print(os.environ.get('DATASET'))
+    print(os.environ.get('USER_TOPIC_1'))
+    print(os.environ.get('USER_TOPIC_2'))
 
-    print("Called with arguments: %s" % args)
+    # TODO this should go in the task definition
+    actual_date = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    bucket_name = f"trend-analysis-{os.environ.get('ENV')}"
+    folder_path = f"input-data/{os.environ.get('DATASET')}"
+    dataset_name = os.environ.get('DATASET').split(".")[0]
+    output_path = f"s3a://trend-analysis-{os.environ.get('ENV')}/output-data/{dataset_name}/{actual_date}"
 
-    bucket_name = f"trend-analysis-{args.env}"
-    folder_path = 'input-data/sample/sample.json'  # TODO this should go in the task definition
-    output_path = "s3a://trend-analysis-dev/output-data/"
-
-    twitter_data = clean_data(sc, bucket_name, folder_path)
+    twitter_data, end_date = clean_data(sc, bucket_name, folder_path, os.environ.get('DATASET'))
     # Define the start and end dates for the last week
-    end_date = datetime(2011, 2, 27)
     start_date = end_date - timedelta(days=7)
-    date_range = [start_date + timedelta(days=i) for i in range(7)]
-    date_df = sc.createDataFrame([(date,) for date in date_range], ["date"]) \
-        .withColumn("date", date_format(col("date"), "yyyy-MM-dd"))
 
-    date_df.show()
-    date_columns = date_df.rdd.flatMap(lambda x: x).collect()
-    print(" end_date : " + str(end_date))
-    print(" start_date : " + str(start_date))
-
-    # Filter tweets from the last week
-    # twitter_data = twitter_data.filter((col("timestamp") >= start_date) & (col("timestamp") <= end_date))
     tokenizer = Tokenizer(inputCol="tweet_text", outputCol="words")
     stop_words_en = StopWordsRemover().getStopWords()
     stop_words_es = ["este", "es", "un", "de", "en", "o", "que", "el", "ella", "ellos", "ellas", "y", "e", "u", "ya",
@@ -137,7 +123,7 @@ if __name__ == '__main__':
     filtered_data = twitter_data.withColumn("timestamp", date_format(col("timestamp"), "yyyy-MM-dd")) \
         .filter((col("timestamp") >= start_date) & (col("timestamp") <= end_date)) \
         .select("timestamp", explode(col("filtered_words")).alias("word")) \
-        .filter((col("word") == "love") | (col("word") == "like")) \
+        .filter((col("word") == os.environ.get('USER_TOPIC_1')) | (col("word") == os.environ.get('USER_TOPIC_2'))) \
         .groupBy("timestamp", "word").agg(count("*").alias("occurrences")) \
         .orderBy(col("occurrences").desc())
 
@@ -150,4 +136,4 @@ if __name__ == '__main__':
 
     pivot_df.show()
 
-    # pivot_df.write.mode("overwrite").csv(output_path, header=True)
+    pivot_df.write.mode("overwrite").csv(output_path, header=True)
